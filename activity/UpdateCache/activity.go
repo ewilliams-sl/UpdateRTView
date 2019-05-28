2aaa64111e54b945df476d15377a2bfc7ccad1de
ewilliams-sl <everett.williams@sl.com>
2019-05-28 15:15:10 -0700

    UpdateCache

--- activity/UpdateCache/activity.go
+++ activity/UpdateCache/activity.go
@@ -187,7 +187,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 //	rslt := string(cmdOut)
 	// Set the result as part of the context
 //	context.SetOutput(result, rslt)
-    a.callPost()
+    &a.callPost()
 
 	// Signal to the Flogo engine that the activity is completed
 	return true, nil
