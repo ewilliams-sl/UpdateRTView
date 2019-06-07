caee1ae11db453fb59c76866ef0540c36e7d5008
ewilliams-sl <everett.williams@sl.com>
2019-06-07 07:12:02 -0700

    Modified the columns in the cache.

--- activity/UpdateCache/activity.go
+++ activity/UpdateCache/activity.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"bytes"
 	"io/ioutil"
+	"os"
 )
 
 // Constants
@@ -84,60 +85,25 @@ func (rowData *slPost) postRowData(url string) error {
 }
 
 
-func (a *MyActivity) callPost(sURL string) {
+
+func (a *MyActivity) updatePerformance(sURL string,  sFlowName string, iMillis int64) {
 	var post slPost
 	post.addCol("time_stamp", "date")
-	post.addCol("connection", "string")
-	post.addCol("objectExtIdTableSize", "int")
-	post.addCol("objectTableSize", "int")
-	post.addCol("expired", "boolean")
-	
-	t2 := makeTimestamp()
-	
-
-	var d map[string]interface{}
-	d = make(map[string]interface{})
-	d["time_stamp"] = t2
-	d["connection"] = "new51Cache"
-	d["objectExtIdTableSize"] = 10000
-	d["objectTableSize"] = 1000
-	d["expired"] = false
-	var d3 map[string]interface{}
-	d3 = make(map[string]interface{})
-	d3["time_stamp"] = t2
-	d3["connection"] = "newbe4cache"
-	d3["objectExtIdTableSize"] = 20000
-	d3["objectTableSize"] = 2000
-	d3["expired"] = false
-
-	var d2 []map[string]interface{}
-	d2 = make([]map[string]interface{}, 2,6)
-	d2[0] = d
-	d2[1] = d3
-	
-	post.Data = d2
-	bolD, _ := json.Marshal(post)
-//	test("http://localhost:5000/rtview/json/data/TbeObjectTable")
-//	test("http://localhost:8085/rtview/json/data/TbeObjectTable")
-
-//	post.postRowData("http://10.0.0.20:5000/rtview/write/cache/TbeObjectTable")
-	post.postRowData(sURL)
-//	post.postRowData("http://localhost:8085/rtview/write/cache/TbeSlop")
-	fmt.Println(string(bolD))	
-}
-
-func (a *MyActivity) updatePerformance(sURL string, sFlowName string, iMillis int64) {
-	var post slPost
-	post.addCol("time_stamp", "date")
+	post.addCol("hostName", "string")
 	post.addCol("flowName", "string")
 	post.addCol("duration", "int")
 	
 	t2 := makeTimestamp()
 	
-
+	sHostName, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	
 	var d map[string]interface{}
 	d = make(map[string]interface{})
 	d["time_stamp"] = t2
+	d["hostName"] = sHostName
 	d["flowName"] = sFlowName
 	d["duration"] = iMillis
 
@@ -168,68 +134,18 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
-	//ivMsg := `{"cmd":"./test.sh","params":"aaa bbb ccc}`
-	// put input varable into a slice (note not order guarenteed)
 	sTarget := context.GetInput(command).(string)
 	fmt.Println(sTarget)
+	
 	sTableDef := context.GetInput(tblDef).(string)
 	fmt.Println(sTableDef)
-//	sData := context.GetInput(tblData).(string)
 
 	fmt.Println(sTableDef)
 	iVal := context.GetInput(spStartTime).(int64)
 	sName := context.ActivityHost().Name()
 	iEndVal := makeTimestamp() -iVal
 	
-//    a.updatePerformance(sTarget, sName, iVal)
     a.updatePerformance(sTarget, sName, iEndVal)
-
-
-	// get the command to execute including path
-//	cmd, ok := ivCmdParams[command].(string) // this should be the command or script to execute
-//	fmt.Println(cmd);
-//	if ok == false {
-//		// no Command to execute
-//		log.Infof("No Command to execute - check input syntax: [%s]", err)
-//		context.SetOutput(result, err.Error())
-//		return true, err
-//	}
-	// We should have the command to execture - check its there
-//	_, err = os.Stat(cmd)
-
-//	if err != nil {
-	// If the file doesn't exist return error
-//		context.SetOutput("result", err.Error())
-//		log.Infof("File [%s] does not exist", cmd)
-//		return true, err
-//	}
-
-	// Get the Commands Params
-//	var paramsArray [20]string                    // FIX THIS: make dynamic but ordered
-//	cmdParams, ok := ivCmdParams[params].(string) // this is a string containg space separated parameters
-//	if ok == false {
-		// no params
-//		log.Infof("No params provided")
-//	} else {
-		// Put into array for exec.Command to use, space separated
-		// put command arguments into an array in the order they are entered.  Order is important.
-//		i := 0
-//		for _, field := range split(cmdParams, ' ') {
-//			paramsArray[i] = field
-//			i++
-//		}
-//	}
-
-	// launch the command
-//	var cmdOut []byte
-//	if cmdOut, err = exec.Command(cmd, paramsArray[0:]...).Output(); err != nil {
-//		log.Infof("Error running Flogo setQoS activity: [%s]", err)
-//		context.SetOutput(result, err.Error())
-//		return true, err
-//	}
-//	rslt := string(cmdOut)
-	// Set the result as part of the context
-//	context.SetOutput(result, rslt)
 
 	// Signal to the Flogo engine that the activity is completed
 	return true, nil
